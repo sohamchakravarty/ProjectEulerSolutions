7b95c0803936bd630365340eb339e22ed0e93466
sohamchakravarty <soham.bonny@gmail.com>
2014-05-07 12:10:17 +0530

    Problem 3: Largest prime factor (225ms)
    Link: http://projecteuler.net/problem=3

--- 003.py
+++ 003.py
@@ -1,14 +1,5 @@
 from math import sqrt
-from time import time
-
-#Factors of a number 'n' excluding 1 and n
-def factors(n):    
-    return set(reduce(list.__add__, 
-                ([i, n/i] for i in range(2, int(sqrt(n)) + 1) if n % i == 0)))
-
-#Function to check if a number is prime
-def isPrime(n):
-     return all(n%i for i in xrange(2,int(sqrt(n))+1))
+from helperFunctions import *
 
 def LargestPrimeFactor(N):
     fctrs = sorted(factors(N))
